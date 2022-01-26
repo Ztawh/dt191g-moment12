0d938cb5108c824232b532ef25fa2bf4c150be02
Amanda Hwatz Björkholm <amandahwatz@gmail.com>
2022-01-26 18:32:19 +0100

    Add styling

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,7 +1,25 @@
 "use strict";
 
-let date = document.getElementById("date");
+// Get elements
+let home = document.getElementById("home");
+let razor = document.getElementById("razor");
+let about = document.getElementById("about");
 
-date.addEventListener("click", () =>{
-    alert("hej där");
-})
+// Get current location
+let locationUrl = window.location.href;
+
+// If current location matches, then add active class. Else add active class to home-page
+if(locationUrl.includes("About", 0)){
+    about.classList.add("active");
+    home.classList.remove("active");
+    razor.classList.remove("active");
+} else if(locationUrl.includes("Razor", 0)){
+    about.classList.remove("active");
+    home.classList.remove("active");
+    razor.classList.add("active");
+} else {
+    about.classList.remove("active");
+    home.classList.add("active");
+    razor.classList.remove("active");
+}
+
